2e49eea84f9b99af1a9b7876de1b0ab6cdbf05db
hoangmaicuong99@gmail.com <hoangmaicuong99@gmail.com>
2022-06-12 16:37:08 +0700

    Ex: Data type Sets

---
+++ RedisProject/Controllers/DataTypeRedis/SetTypeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisProject.Controllers.DataTypeRedis
+{
+    [Route("api/redis/[controller]")]
+    [ApiController]
+    public class SetTypeController : ControllerBase
+    {
+        readonly IDatabase _dbRedis;
+        public SetTypeController(IDatabase dbRedis)
+        {
+            _dbRedis = dbRedis;
+        }
+        [HttpGet("Get")]
+        public async Task<string> Get(string key)
+        {
+            try
+            {
+                key = $"Set_Type:{key}";
+                var result = await _dbRedis.SetMembersAsync(key);
+                return string.Join("--", result);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            return $"";
+        }
+
+        [HttpPost("Create")]
+        public async Task<string> Create([FromForm]string key = null, [FromForm] string val= null)
+        {
+            try
+            {
+                key = $"Set_Type:{key}";
+                var result = await _dbRedis.SetAddAsync(key, val);
+                return $"{result}";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
